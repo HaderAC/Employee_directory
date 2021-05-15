4d49eb5eaeac95ddd7a01b619e20f1396387a4d8
HaderCore <Hadercortes@gmail.com>
2021-05-15 15:17:12 -0400

    added Header and css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Table from "./components/Table";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div >
-
+      <Header><h1>Employee Directory</h1></Header><Header/>
+      <Table/>
     </div>
   );
 }
